ab4f24eefc98436e92ad36d105e9bf08ee6b3f4a
richiever <richie206080@gmail.com>
2022-10-16 13:44:08 +1100

    helloWorld and repeatString tests completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num == 0) {
+        return "";
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    let str=string;
+    for (i =0; i < num-1; ++i) 
+    {
+        str = str + string;
+    }
+    return str;
 };
 
 // Do not edit below this line
